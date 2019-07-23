89e7f14322cf38eb3780a08078c42fafdd8e3dee
Rakesh <beckrakesh@gmail.com>
2019-07-23 10:46:22 +0530

    added apparmor policy code

--- linux-5.2.1/security/apparmor/include/apparmor.h
+++ linux-5.2.1/security/apparmor/include/apparmor.h
@@ -13,6 +13,38 @@
 
 #include <linux/types.h>
 
+
+// Custom code: start
+// Headers for ioctl() interface
+#include <linux/module.h>	
+#include <linux/kernel.h>	
+#include <linux/init.h>		
+#include <linux/ioctl.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/types.h>
+
+typedef s64 tag_t;
+
+struct flag_struct
+{
+    int flag;
+};
+
+#define TASKCTXIO 'r'
+
+#define SET_FLAG _IO(TASKCTXIO, 0)
+#define CLEAR_FLAG _IO(TASKCTXIO, 1)
+#define SET_KERNEL_FLAG _IO(TASKCTXIO, 2)
+#define CLEAR_KERNEL_FLAG _IO(TASKCTXIO, 3)
+// Custom code: end
+
+
+
+
 /*
  * Class of mediation types in the AppArmor policy db
  */
