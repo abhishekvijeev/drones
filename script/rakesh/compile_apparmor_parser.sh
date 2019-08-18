47d436daf13d88c23e21fc029ccaf4b2e9b538b7
Abhishek Vijeev <abhishek.vijeev@gmail.com>
2019-08-18 14:35:02 +0530

    added apparmor compile scrite

---
+++ script/rakesh/compile_apparmor_parser.sh
@@ -0,0 +1,37 @@
+cd /home/abhishek/drones/apparmor-apparmor-2.13/libraries/libapparmor
+./autogen.sh
+sh ./configure --prefix=/usr --with-perl --with-python
+make
+#make check
+make install
+
+'''
+#Binary Utilities:
+cd /home/abhishek/linux/apparmor-apparmor-2.13/binutils
+make
+make check
+make install
+'''
+
+
+ #parser:
+cd /home/abhishek/drones/apparmor-apparmor-2.13/parser
+make      # depends on libapparmor having been built first
+#make check
+make install
+
+
+'''
+#Utilities:
+
+cd /home/abhishek/linux/apparmor-apparmor-2.13/utils
+make
+make check
+make install
+
+#Apache mod_apparmor:
+
+cd /home/abhishek/linux/apparmor-apparmor-2.13/changehat/mod_apparmor
+make      # depends on libapparmor having been built first
+make install
+'''
