a226588810e388984f9da44f017e87709d0f6d7b
Abhishek Vijeev <abhishek.vijeev@gmail.com>
2019-09-09 13:14:16 +0530

    bug fix - 1

--- linux-5.2.1/security/apparmor/lsm.c
+++ linux-5.2.1/security/apparmor/lsm.c
@@ -1841,29 +1841,25 @@ static void apparmor_shm_free_security(struct kern_ipc_perm *perm)
 	// }
 	// aa_put_label(curr_label);
 
-	struct aa_profile *profile;
-	struct aa_label *curr_label;
-	char *curr_domain = NULL;
-	int curr_domain_len = 0;
-	curr_label = __begin_current_label_crit_section();
-	fn_for_each (curr_label, profile, apparmor_getlabel_domain(profile, &curr_domain));
-	__end_current_label_crit_section(curr_label);
-
 
 	printk(KERN_INFO "apparmor_shm_free_security (%s)\n", current->comm);
-
 	struct ListOfDomains *perm_security_list = (struct ListOfDomains *)perm->security;
-	struct ListOfDomains *iterator, *tmp;
-	iterator = list_first_entry(&(perm_security_list->domain_list), typeof(*iterator), domain_list);
-	while((&iterator->domain_list) != &(perm_security_list->domain_list))
+
+	if(perm_security_list)
 	{
-		tmp = iterator;
-		iterator = list_next_entry (iterator, domain_list);
-		printk(KERN_INFO "apparmor_shm_free_security (%s): Freeing list node %s\n", current->comm, tmp->domain);
-		kzfree (tmp->domain);
-		kzfree (tmp);
-	}	
-	kzfree(perm_security_list);
+		struct ListOfDomains *iterator, *tmp;
+		iterator = list_first_entry(&(perm_security_list->domain_list), typeof(*iterator), domain_list);
+		while((&iterator->domain_list) != &(perm_security_list->domain_list))
+		{
+			tmp = iterator;
+			iterator = list_next_entry (iterator, domain_list);
+			printk(KERN_INFO "apparmor_shm_free_security (%s): Freeing list node %s\n", current->comm, tmp->domain);
+			kzfree (tmp->domain);
+			kzfree (tmp);
+		}	
+		kzfree(perm_security_list);
+	}
+	
 	
 	
 }
