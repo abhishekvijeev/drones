60ad0ac03cb5b5682eda154ab90edbd5bddf0607
Abhishek Vijeev <abhishek.vijeev@gmail.com>
2019-10-09 19:43:28 +0530

    print data for unix socket domain

--- linux-5.2.1/security/apparmor/lsm.c
+++ linux-5.2.1/security/apparmor/lsm.c
@@ -1690,6 +1690,9 @@ static int apparmor_socket_sendmsg(struct socket *sock,
 			else if(sk->sk_family == AF_UNIX)
 			{
 				printk (KERN_INFO "apparmor_socket_sendmsg: UNIX DOMAIN SOCKET \n");
+				printk (KERN_INFO "apparmor_socket_sendmsg: address pair = %ld, port_pair = %d \n", sk->sk_addrpair, sk->sk_portpair);
+				printk (KERN_INFO "apparmor_socket_sendmsg: desti addr = %d, desti port = %d,  sk_rcv_saddr = %d, sk_num = %d \n", sk->sk_daddr, 														sk->sk_dport, sk->sk_rcv_saddr, sk->sk_num);
+				
 			}
 		
 		}//end if (curr_domain != NULL)
@@ -1788,6 +1791,9 @@ static int apparmor_socket_recvmsg(struct socket *sock,
 			else if(sock->sk->sk_family == AF_UNIX)
 			{
 				printk (KERN_INFO "apparmor_socket_recvmsg: UNIX DOMAIN SOCKET \n");
+				printk (KERN_INFO "apparmor_socket_recvmsg: address pair = %ld, port_pair = %d \n", sk->sk_addrpair, sk->sk_portpair);
+				printk (KERN_INFO "apparmor_socket_recvmsg: desti addr = %d, desti port = %d,  sk_rcv_saddr = %d, sk_num = %d \n", sk->sk_daddr, 														sk->sk_dport, sk->sk_rcv_saddr, sk->sk_num);
+				
 			}
 		} // end for if (curr_domain != NULL)	
 		aa_put_label(curr_sock_label);
