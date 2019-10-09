83661a910c156e4479041ee77476b07e5c14f4ac
Abhishek Vijeev <abhishek.vijeev@gmail.com>
2019-10-09 19:56:42 +0530

    bug fix 2

--- linux-5.2.1/security/apparmor/lsm.c
+++ linux-5.2.1/security/apparmor/lsm.c
@@ -1791,8 +1791,8 @@ static int apparmor_socket_recvmsg(struct socket *sock,
 			else if(sock->sk->sk_family == AF_UNIX)
 			{
 				printk (KERN_INFO "apparmor_socket_recvmsg: UNIX DOMAIN SOCKET \n");
-				printk (KERN_INFO "apparmor_socket_recvmsg: address pair = %lld, port_pair = %d \n", sk->sk_addrpair, sk->sk_portpair);
-				printk (KERN_INFO "apparmor_socket_recvmsg: desti addr = %d, desti port = %d,  sk_rcv_saddr = %d, sk_num = %d \n", sk->sk_daddr, 														sk->sk_dport, sk->sk_rcv_saddr, sk->sk_num);
+				printk (KERN_INFO "apparmor_socket_recvmsg: address pair = %lld, port_pair = %d \n", sock->sk->sk_addrpair, sock->sk->sk_portpair);
+				printk (KERN_INFO "apparmor_socket_recvmsg: desti addr = %d, desti port = %d,  sk_rcv_saddr = %d, sk_num = %d \n", sock->sk->sk_daddr, 														sock->sk->sk_dport, sock->sk->sk_rcv_saddr, sock->sk->sk_num);
 				
 			}
 		} // end for if (curr_domain != NULL)	
